7d9b1c568b1a90e2ad703d2fe1047046bcd3d94b
Loris Galleana <165955421+LorisGalleana@users.noreply.github.com>
2024-09-28 15:25:28 +0200

    Initial commit

---
+++ resources/views/admin/projects/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @if(session('message'))
+        <div class="alert alert-success">{{ session('message') }}</div>
+    @endif
+    <h1>Dettagli progetto</h1>
+    <div>
+        <a href="{{ route('admin.projects.edit', ['project' => $project->id]) }}" class="btn btn-secondary">Modifica</a>
+        @include('admin.partials.formdelete', [
+                            'route' => route('admin.projects.destroy', $project),
+                            'message' => "Confermi l\'eliminazione del progetto: $project->title ?"
+                            ])
+    </div>
+    <ul>
+        <li>Titolo: {{ $project->title }} </li>
+        <li>Descrizione: {{ $project->text }} </li>
+        <li>Data creazione: {{ ( $project->created_at )->format('d/m/Y') }} </li>
+        <li>Data ultima modifica: {{ ( $project->updated_at )->format('d/m/Y') }} </li>
+    </ul>
+    <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">Torna all'elenco</a>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Guest\PageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [PageController::class, 'index'])->name('home');
+
+Route::middleware(['auth', 'verified'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('home');
+        Route::resource('projects', ProjectController::class);
+    });
+
+Route::middleware('auth')
+    ->prefix('profile')
+    ->name('profile.')
+    ->group(function () {
+        Route::get('/', [ProfileController::class, 'edit'])->name('edit');
+        Route::patch('/', [ProfileController::class, 'update'])->name('update');
+        Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
+});
+
+require __DIR__.'/auth.php';
